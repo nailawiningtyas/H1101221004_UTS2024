417387d3c4daa28960fa0504d172471b14d2a4b0
nailawiningtyas <naila.pradanti@gmail.com>
2024-03-29 21:35:59 +0700

    update kode baru lagi

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\home;
-use App\Http\Controllers\pegawai;
-use App\Http\Controllers\jabatan;
-use App\Http\Controllers\pendidikan;
+use App\Http\Controllers\Home;
+use App\Http\Controllers\Jabatan;
+use App\Http\Controllers\Pendidikan;
+use App\Http\Controllers\Pegawai;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [Home::class, 'index']);
+Route::get('/home', [Home::class, 'index']);
+Route::get('/jabatan', [Jabatan::class, 'index']);
+Route::get('/pendidikan', [Pendidikan::class, 'index']);
+Route::get('/pegawai', [Pegawai::class, 'index']);
 
-Route::get('/', [home::class, 'index']);
-Route::get('/home', [home::class, 'index']);
-Route::get('/pegawai', [pegawai::class, 'index']);
-Route::get('/jabatan', [jabatan::class, 'index']);
-Route::get('/pendidikan', [pendidikan::class, 'index']);
